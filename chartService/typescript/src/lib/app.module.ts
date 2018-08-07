d32cc10844e29cc3dbcb99d0e1bcf3dd88efcdc2
edenpan <352007311@qq.com>
2018-08-07 15:03:36 +0800

    delete the unwork code

--- chartService/typescript/src/lib/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatafeedController } from './datafeed.controller';
-import { DatafeedService } from './datafeed.service';
-
-@Module({
-  controllers: [DatafeedController],
-  components: [DatafeedService],
-})
-export class AppModule {}
